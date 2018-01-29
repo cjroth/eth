7ef720ce6543f9fa6fd9f9091656a3e300432db6
Chris Roth <chris@cjroth.com>
2018-01-29 18:27:19 -0500

    tweaks

--- hold.sol
+++ hold.sol
@@ -1,29 +1,55 @@
 pragma solidity ^0.4.0;
+
 contract Hold {
 
     uint withdrawTime = 1517085048;
 
-    struct Holder {
+    struct Holder
+    {
         address holder;
         uint amount;
     }
 
     mapping(address => Holder) holders;
 
-    modifier onlyBefore(uint _time) { require(now < _time); _; }
-    modifier onlyAfter(uint _time) { require(now > _time); _; }
-    modifier hasFunds(_amount) {
-        require(sender.amount >= _amount);
+    modifier onlyBefore(uint _time)
+    {
+        require(now < _time);
+        _;
+
+    }
+
+    modifier onlyAfter(uint _time)
+    {
+        require(now > _time);
+        _;
+
+    }
+
+    modifier hasFunds(uint _hasAmount)
+    {
+        Holder storage sender = holders[msg.sender];
+        require(sender.amount >= _hasAmount);
         _;
     }
 
-    function deposit() public payable onlyBefore(withdrawTime) returns (bool) {
+    function deposit()
+        public
+        payable
+        onlyBefore(withdrawTime)
+        returns (bool)
+    {
         Holder storage sender = holders[msg.sender];
         sender.amount += msg.value;
         return true;
     }
 
-    function withdraw(uint _amount) public onlyAfter(withdrawTime) hasfunds(_amount) returns (bool) {
+    function withdraw(uint _amount)
+        public
+        onlyAfter(withdrawTime)
+        hasFunds(_amount)
+        returns (bool)
+    {
         Holder storage sender = holders[msg.sender];
         sender.amount -= _amount;
         msg.sender.transfer(_amount);
